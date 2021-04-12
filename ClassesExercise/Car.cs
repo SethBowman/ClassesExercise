4b3842f74da4d67a43ba3b950a10d3d743caa89e
Seth Bowman <brandonsethbowman@gmail.com>
2021-04-12 13:54:21 -0500

    initial commit

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise
+{
+    public class Car
+    {
+
+        //Constructors
+
+        public Car() //- a special member method that has the same name as it's class.
+        {
+
+        }
+
+
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+
+
+
+        //Fields
+        //Properties
+        //Methods
+
+
+        // Create a Make property of type string that is public
+        public string Make { get; set; } //get = read, set = write
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dot notation
+            Car myCar = new Car(); // Constructor = special member method. Helps us create a new instance of the 'Car' class
+
+            myCar.Make = "Suzuki"; //Properties
+            myCar.Model = "Forenza";
+            myCar.Year = 2008;
+
+            //object initializer
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+
+            //passing in through constructor
+
+            var chevy = new Car("Chevy", "Impala", 1964);
+
+
+
+            Console.WriteLine(myCar.Make);
+            Console.WriteLine(myCar.Model);
+            Console.WriteLine(myCar.Year);
+
+            Console.WriteLine(mazda.Make);
+            Console.WriteLine(mazda.Model);
+            Console.WriteLine(mazda.Year);
+
+            Console.WriteLine(chevy.Make);
+            Console.WriteLine(chevy.Model);
+            Console.WriteLine(chevy.Year);
+
+
+
+
+
+        }
+    }
+}
